261aef849afb885339af565411f30fc6cfc2121c
patrick mcdermott <patrickmcdermott@patricks-MBP.fios-router.home>
2021-09-20 09:03:15 -0400

    first commit

---
+++ bot.js
@@ -0,0 +1,85 @@
+
+const credentials = require('./credentials');
+
+var https = require('https'); 
+const { threadId } = require('worker_threads');
+const apiKey = process.env.API_KEY || credentials.API_KEY;
+const apiKeySecret = process.env.API_KEY_SECRET || credentials.API_KEY_SECRET;
+const bearerToken = process.env.BEARER_TOKEN || credentials.BEARER_TOKEN;
+
+
+//Creates the URL object from the hostname
+//We will work with this object to hit the various endpoints of the API
+const requestUrl = new URL('https://api.twitter.com');
+
+//path to see specific tweets by id
+const tweetsPath = "/2/tweets";
+
+//path to search for recent tweets
+const searchRecentPath = "/1.1/search/tweets.json";
+
+//set search params 
+const params = new URLSearchParams([
+      
+  //search for hashtag minecraft 
+        ['q','%23minecraft']
+       
+]);
+
+//add params to the URL that we created
+requestUrl.search = params;
+
+//set path for the endpoint we want to hit
+//only one path can be uncommented at a time
+
+//uncomment to search recent tweets
+requestUrl.pathname = searchRecentPath;
+
+//uncomment to get specific tweets by ID
+//requestUrl.pathname = tweetsPath;
+
+//set the bearer token so that Twitter API authenticates us
+const options = {
+    headers: {
+        'authorization': `Bearer ${bearerToken}`
+    }
+}
+
+//This method is passed to the https request as a callback to handle the response
+getTweets = ((response)=> {
+  
+    var str = ''
+
+    response.on('data', (chunk)=> {
+      str+=chunk
+    });
+  
+    //at the end of the response we can build out the logic for what do with the returned tweets
+    response.on('end', ()=> {
+
+      let tweets = JSON.parse(str)
+
+      //uncomment to print the entire reponse to the console
+      //console.log(tweets.statuses)
+      
+
+    //uncomment to print the status text (the Tweet) and the user.
+
+      // console.log(tweets.statuses.forEach(statuses => {
+      //   console.log('user: '+ statuses.user.screen_name + '\t'+'tweet: ' + statuses.text)
+      // }))
+
+      //print a list of screen names that have recently tweeted the hashtag we searched for.
+      console.log(tweets.statuses.forEach(statuses => {
+        console.log(statuses.user.screen_name)
+      }))
+    });
+  });
+
+https.get(requestUrl,options,getTweets);
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,31 @@
+## Overview 
+
+This bot is built in node.js and uses the Twitter API to search twitter programatically. </br>
+I have built this with a minimum of external libraries in order to further explore node.js itself.
+This is also makes the project accessible to those learning to code. I have tried to explain everything step by step in the comments and in this readme. 
+
+## Next steps
+I plan to build this out by building functionality for hitting all the different Twitter API endpoints
+
+
+## Running 
+
+### Pre-requisites
+You will need: </br>
+1. Node.js
+2. NPM
+3. A Twitter Developer account
+
+
+Fork the project and create a branch. You can read how to do so *[here](https://docs.github.com/en/github/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-and-deleting-branches-within-your-repository)* </br>
+
+Create a file called credentials.js to store your bearer token and API keys (obtained from your Twitter Developer Account). 
+</br>Be sure to add credentials.js to .gitignore so that you don't share them to GitHub should you decide to host your repository there.</br>
+Alternately, you can enter these credentials as command line args.
+</br>
+Navigate to the project directory and run <code>npm install</code> </br>
+Once dependencies have finished installing, run <code>node bot.js</code>
+
+
+
+
